73216c1d073b2ec4b24da9e309d74f2502e938e1
Mbabcock-acb <mbabcock@acb.org>
2026-05-17 17:46:53 -0700

    style: apply dart format to app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,34 +2,34 @@ import 'package:flutter/material.dart';
 
 abstract final class AppTheme {
   static ThemeData light() => ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-        ),
-      );
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+    ),
+  );
 
   static ThemeData dark() => ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          brightness: Brightness.dark,
-        ),
-      );
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      brightness: Brightness.dark,
+    ),
+  );
 
   static ThemeData highContrastLight() => ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          contrastLevel: 1,
-        ),
-      );
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      contrastLevel: 1,
+    ),
+  );
 
   static ThemeData highContrastDark() => ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          brightness: Brightness.dark,
-          contrastLevel: 1,
-        ),
-      );
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+      brightness: Brightness.dark,
+      contrastLevel: 1,
+    ),
+  );
 }
